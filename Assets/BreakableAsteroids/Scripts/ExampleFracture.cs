a1e90fd48c6ac99fd244abdbca50bea067c28ef0
Randall Price <randall_price@hotmail.com>
2026-03-02 01:42:51 -0500

    Initial project upload.

---
+++ Assets/BreakableAsteroids/Scripts/ExampleFracture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ExampleFracture : MonoBehaviour
+{
+    public GameObject[] Asteroids;
+    public GameObject   Chunker;
+
+
+    private int  _counter = 0;
+    private bool _isChunkerAvailable = true;
+
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))                       // Old Input Manager code - Space key
+        //if (Keyboard.current[Key.Space].wasPressedThisFrame)       // New Input System  code - Space key
+
+        //if (Mouse.current.leftButtonv.wasPressedThisFrame)         // New Input System code - mouse left click
+        //if (Mouse.current.rightButton.wasPressedThisFrame)         // New Input System code - mouse right click
+
+        try
+        {
+            if (Asteroids.Length > 0)
+            {
+                if (_counter < Asteroids.Length)
+                {
+                    // Use the Space key to loops through asteroids and fractures them.
+                    if (Keyboard.current[Key.Space].wasPressedThisFrame)
+                    {
+                        Asteroids[_counter].GetComponent<Fracture>().FractureObject();
+                        _counter++;
+                    }
+                }
+            }
+
+            if (_isChunkerAvailable)
+            {
+                if (Keyboard.current[Key.I].wasPressedThisFrame)
+                {
+                    _isChunkerAvailable = false;
+                    Chunker.SetActive(true);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            string message = ex.Message;
+         }
+
+    }   // Update()
+
+
+}   // class ExampleFracture
---
+++ Assets/BreakableAsteroids/Scripts/Fracture.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+
+public class Fracture : MonoBehaviour
+{
+    // -------------------------------------------------------------------------
+    // Public Variables:
+    // -----------------
+    //   Fractured
+    // -------------------------------------------------------------------------
+
+    #region .  Public Variables  .
+
+    [Tooltip("\"Fractured\" is the object that this will break into")]
+    public GameObject Fractured;
+
+    #endregion
+
+
+
+    // -------------------------------------------------------------------------
+    // Public Methods:
+    // ---------------
+    //   FractureObject
+    // -------------------------------------------------------------------------
+
+    #region .  FractureObject()  .
+    // -------------------------------------------------------------------------
+    //   Method.......:  FractureObject()
+    //   Description..:  
+    //   Parameters...:  None
+    //   Returns......:  Vector3
+    // -------------------------------------------------------------------------
+    public void FractureObject()
+    {
+        //print($"Fracture.FractureObject():  Fractured.name = {Fractured.name}, transform.position = {transform.position}");
+
+        // Spawn the broken version of this asteroid.
+        Instantiate(Fractured, transform.position, transform.rotation);
+
+        // Hide this asteroid so it doesn't get in the way.
+        //gameObject.SetActive(false);
+
+        // Destroy this asteroid so it doesn't get in the way.
+        Destroy(gameObject);
+
+    }   // FractureObject()
+    #endregion
+
+
+}   // class Fracture
